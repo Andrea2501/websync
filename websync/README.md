397cebad6f77997f533d7f245799cc2a59766e89
Andrea2501 <aberni.2501@gmail.com>
2023-11-19 10:27:37 +0100

    Aggiornamanto File Readme - Lettura e Controllo configurazione Web Sync Con Api

---
+++ websync/README.md
@@ -0,0 +1,122 @@
+# WEB SYNC V 1.0 BETA
+
+Plugin per sincronizzazione con sorgente dati esterne.
+Attualmente in sviluppo:
+Prodotti
+Brand categorie
+
+## Come utilizzare
+
+Compilare per prima cosa i nomi campi sorgente dei dati esterni, se non si hanno i nomi dei campi si può utilizzare la numerazione partendo da 0 (solo csv). NON SI POSSONO UTILIZZARE ENTRAMBI.
+
+Dopo aver compilato i campi, compilare le regole: i dati verranno presi dalle regole quindi è obbligatorio compilare le regole. Utilizzare un nome della regola ed associare i campi alla regola che verrà letta al momento dell'importazione
+
+Terminato di compilare le regole, effettuare il BINDING delle regole hai rispettivi campi delle tabelle di appoggio. I campi delle tabelle di appoggio sono già inseriti, non si possono aggiungere nuovi campi.
+Selezionare il campo della tabella d'appoggio quindi, effettuare il binding dei campi definiti nelle regole. I campi delle tabelle di appoggio hanno già definito il tipo di dato che si aspetta e che è conforme a quello che si aspetta OFFLINE MALL.
+
+### IMPORTANTE
+
+Le chiamate alle api prevedono parametri opzionali. I parametri opzionali devono essere aggiunti nelle configurazione e si riferiscono a:
+    . nome parametro numero record per pagina
+    . valore parametro numero record per pagina
+    . pagina da leggere 
+    . utilizzare la data di update per leggere i dati modificati solo dopo l'ultimo parametro', questo parametro deve essere aggiunto come campo delle tabelle ed associato ad un regola: il valore di questo parametro deve avere specificata la regola "campo data update". I valori delle date contenuti nei dati delle api devono essere in formato: Y-m-d I:h:s esempio 2023-10-27 23:54:00.     
+
+
+
+Sono state aggiunti i modelli e i controller per le tabelle d'appoggio con prefisso support_ per categorie, brand e prodotti
+.
+.
+.
+.
+
+## Preparazioni model e controller per Csv e Api
+
+Impostare le configurazioni prima di eseguire ogni importazione
+
+## CSV
+
+    Configurare il percorso di download dei file csv, se non verranno eseguite modifiche i file dovranno essere in /storage/app/media/websync/. Se si desidera specificare un percorso diverso e si desidera salvare in  /storage/app/media/csvfiles/, scrivere esclusivamente csvfiles.
+
+    Specificare i nomi dei rispettivi file che verranno caricati o scaricati tramite ftp.
+
+    Parametri: 
+    Al momento in via di definizione: i file verranno caricati o direttamente nella cartella specificata o tramite ftp e verrà effettuato il chunk dei record direttamante dal codice sviluppato.
+
+    FTP:
+    Se presente un accesso ftp i file verranno scaricati nella cartella locale specificata in precedenza.
+    Parametri obbligatori:
+    ip del server (host):
+    username
+    password
+    porta
+    SSL obbligatorio.
+
+    l'Ftp dovrà puntare direttamente alla cartella contenente i file csv che verranno scaricati in locale, i nomi dei file deve essere esattamente quello specificato nelle configurazioni e potranno contenere solo lettere, numeri e underscore [AZaz0-9_].csv
+
+## API
+
+Le api sono state relizzate secondo il modello standard e comunque più chiaro per una struttura astratta.
+Riport l'esempio di una chiamata api. Attenzione: i nomi dei campi dell' oggeto contenente i valori non DEVONO CONENERE SPAZIO. Esempio: Nome Prodotto, non è valido; Nome_Prodotto è un nome valido.
+I nomi dei campi possono contenere solo lettere numeri e underscore . Non POSSONO INIZIARE CON UN underscore ed è preferibile che inizino con una lettera.
+
+Chiamata di esempio che verrà utilizzato nel codice
+
+```php
+
+//funzione per ottenere il token
+protected function getToken($username, $password)
+    {
+        $urlForToken = "https://catalogo.specialformaggi.it/services/export/login"; // url alla chiamata per ottenere il token
+
+        //parametri che si aspetta la chiamata: username e password che vengono specificati nelle configurazioni
+        $response = Http::post($urlForToken, [
+            'username' => $username, 
+            'password' => $password,
+        ]);
+        if ($response->ok()) {
+            return $response->body(); 
+            // risposta dalla api se va a buon fine che ci deve restituire il token es: "abcdefgh" come stringa del body e che useremo per le rimanente chimate api
+        } else {
+            return null;
+        }
+    }
+
+
+
+
+protected function getArticles($token,$date)
+    {
+        $urlArticoli = "https://catalogo.specialformaggi.it/services/export/articoli"; //url dell'Api ai prodotti
+
+        // parametri da passare alle api, in questo caso stiamo usando il token
+        // il nome dei parametri vanno specificati nella configurazione
+        $curl_post_data = [
+            "token" => $token, // token è il parametro che si aspetta l'api e $token è il valore
+            "dataagg"=>$date  // dataaagg è il parametro di data da cui iniziamo a leggere i dati $date è il valore  
+        ];
+
+        $data = json_encode($curl_post_data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urlArticoli);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 1110);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json')); // ci aspettiamo sempre una risposta json
+        $curl_response = curl_exec($ch);
+
+        $result = json_decode($curl_response, true);
+        if ($result && is_array($result) && count($result) > 0) {
+            return $result["articoli"]; 
+            // "articoi" è il nome dell'oggetto che contiene tutti recordi ottenuti dalla chiamata e va specificato nelle configurazioni
+        } else {
+            return null;
+        }
+    }
+
+```
+ 
+
+    
--- websync/components/TestLetturaModel.php
+++ websync/components/TestLetturaModel.php
@@ -4,6 +4,7 @@ use Cms\Classes\ComponentBase;
 use Tecnotrade\Websync\Models\ClientProductRules as ClientProductRules;
 use Tecnotrade\Websync\Models\ClientProductFields as ClientProductFields;
 use Tecnotrade\Websync\Models\TableProductFields as SincroProductFields;
+use Tecnotrade\Websync\Models\ConfigSetting As SyncSetting;
 
 use October\Rain\Exception\ApplicationException as AppException;
 
@@ -28,12 +29,24 @@ class TestLetturaModel extends ComponentBase
     // Rest of your code
     }
 
+    public $regoleToBind=array();
+    public $fieldBind=array();
+    public $csvConfiguration=array();
+    public $apiConfiguration=array();
+    public $tipoClientData;
+
     public function onRun(){
+        $this->tipoClientData=$this->getGeneralConfiguration();
+        if($this->tipoClientData=="API"){
+            $this->apiConfiguration=$this->getApiConfiguration();
+        }
+        else{
+            $this->csvConfiguration=$this->getCsvConfiguration();
+        }
         $this->prepareProductsRules();
     }
 
-    public $regoleToBind=array();
-    public $fieldBind=array();
+    
 
     public function prepareProductsRules() {
         $allProductFields=SincroProductFields::all();
@@ -93,5 +106,178 @@ class TestLetturaModel extends ComponentBase
 
             throw new AppException('Non hai effettuato il binding corretto con la tabella prodotti');
         }
+    }
+
+    protected function getGeneralConfiguration(){
+        if(SyncSetting::get('dropdown1')){
+            $tipoClientData=SyncSetting::get('dropdown1');
+            return $tipoClientData;
+        }
+        else{
+            throw new AppException('Non hai creato le configurazioni per il plugin');
+        }
+        
+    }
+    protected function getApiConfiguration(){
+        $username='';
+        $password='';
+        $tokenRequired=0;
+        $urlLetturaToken='';
+        $postParamNameToken='';
+        $postParamNamePassword='';
+        $postParamNameUsername='';
+        $postParamNamePage='';
+        $postParamNamePerPageRecord='';
+        $postParmNumRecordPerPage=0;
+        $postParamDataUpdateName=null;
+        $chiamataApiProdotti='';
+        $chiamataApiCategorie='';
+        $chiamataApiBrand='';
+        $variabileResultsProdotti='';
+        $variabileResultsCategorie='';
+        $variabileResultsBrands='';
+        
+
+        
+        if(SyncSetting::get('api_username') && SyncSetting::get('api_password')){
+        $username=SyncSetting::get('api_username');
+        $password=SyncSetting::get('api_password');
+        }
+        else{
+            throw new AppException('Non hai creato le credenziali per le API da utilizzare');
+        }
+        if(SyncSetting::get('api_token_required')){
+            $tokenRequired=SyncSetting::get('api_token_required');
+            
+        }
+        if($tokenRequired=="1"){
+            if(SyncSetting::get('api_url_per_ottenere_token')){
+                $urlLetturaToken=SyncSetting::get('api_url_per_ottenere_token');
+            }
+            else{
+                throw new AppException('Hai Impostato autenticazione tramite TOKEN ma non hai specificato la Chiamata per ottenre il TOKEN');
+            }
+            if(SyncSetting::get('api_param_post_token_name')){
+                $postParamNameToken=SyncSetting::get('api_param_post_token_name');
+            }
+            else{
+                throw new AppException('Non hai specificato il nome del parametro per passare il token alle chiamate API');
+            }
+            
+            
+        }
+        if(SyncSetting::get('api_post_param_username_name') &&  SyncSetting::get('api_post_param_password_name')){
+            $postParamNamePassword=SyncSetting::get('api_post_param_password_name');
+            $postParamNameUsername=SyncSetting::get('api_post_param_username_name');
+            
+        }
+        else{
+            throw new AppException('Non hai specificato il nome del parametro per passare la password o lo username alle API');
+        }
+        if(SyncSetting::get('api_post_page_param_name')){
+            $postParamNamePage=trim(SyncSetting::get('api_post_page_param_name'))==''? null : SyncSetting::get('api_post_page_param_name');
+        }
+        else{
+            $postParamNamePage=null;
+        }
+        if(SyncSetting::get('api_post_per_page_param_name')){
+            $postParamNamePerPageRecord=trim(SyncSetting::get('api_post_per_page_param_name'))==''? null : SyncSetting::get('api_post_page_param_name');
+            
+        }
+        else{
+            $postParamNamePerPageRecord=null;
+        }
+        if(SyncSetting::get('api_num_record_per_page')){
+            $postParmNumRecordPerPage=SyncSetting::get('api_num_record_per_page');
+        }
+        else{
+            $postParmNumRecordPerPage=null;
+        }
+        if(SyncSetting::get('api_post_date_update_param_nam')){
+            $postParamDataUpdateName=SyncSetting::get('api_post_date_update_param_nam');
+        }
+        else{
+            $postParamDataUpdateName=null;
+        }
+        if(SyncSetting::get('api_toproducts')){
+            $chiamataApiProdotti=SyncSetting::get('api_toproducts');
+        }
+        else{
+            throw new AppException('Non hai specificato l\'url della Api Alla chimata dei prodotti.');
+            
+        }
+        if(SyncSetting::get('api_tocategories')){
+            $chiamataApiCategorie=SyncSetting::get('api_tocategories');
+        }
+        else{
+            throw new AppException('Non hai specificato l\'url della Api Alla chimata delle categorie.');
+            
+        }
+        if(SyncSetting::get('api_tobrands')){
+            $chiamataApiBrand=SyncSetting::get('api_tobrands');
+        }
+        else{
+            throw new AppException('Non hai specificato l\'url della Api Alla chimata dei brand.');
+        }
+        if(SyncSetting::get('api_toproducts_result_name')){
+            $variabileResultsProdotti=SyncSetting::get('api_toproducts_result_name');
+        }
+        else{
+            throw new AppException('Non hai specificato la variabile contenente i risultati resituiti dalla chiamata ai prodotti');
+
+        }
+        if(SyncSetting::get('api_tocategories_result_name')){
+            $variabileResultsCategorie=SyncSetting::get('api_tocategories_result_name');
+        }
+        else{
+            throw new AppException('Non hai specificato la variabile contenente i risultati resituiti dalla chiamata alle categorie');
+            
+        }
+        if(SyncSetting::get('api_tobrands_result_name')){
+            $variabileResultsBrands=SyncSetting::get('api_tobrands_result_name');
+        }
+        else{
+            throw new AppException('Non hai specificato la variabile contenente i risultati resituiti dalla chiamata ai brand');
+            
+        }
+        
+        $this->checkEmptyConfiguration($username,'Username');
+        $this->checkEmptyConfiguration($password,'Password');
+        if($tokenRequired){
+            $this->checkEmptyConfiguration($tokenRequired,'Specifa richiesta token');
+            if($tokenRequired=="1"){
+                $this->checkEmptyConfiguration($urlLetturaToken,'Url lettura token');
+                $this->checkEmptyConfiguration($postParamNameToken,'Nome parametro da passare per ottenre il token');
+            }
+        }
+        $this->checkEmptyConfiguration($postParamNamePassword,'Nome parametro contenente la password');
+        $this->checkEmptyConfiguration($postParamNameUsername,'Nome parametro contenente lo username');
+        $this->checkEmptyConfiguration($chiamataApiProdotti,'Non hai inserito url chiamata a Api Prodotti');
+        $this->checkEmptyConfiguration($chiamataApiCategorie,'Non hai inserito url chiamata a Api Categorie');
+        $this->checkEmptyConfiguration($chiamataApiBrand,'Non hai inserito url chiamata a Api Brand');
+        $this->checkEmptyConfiguration($variabileResultsProdotti,'Non hai inserito la variabile contente i risultati della chiamata a Api prodotti');
+        $this->checkEmptyConfiguration($variabileResultsCategorie,'Non hai inserito la variabile contente i risultati della chiamata a Api categories');
+        $this->checkEmptyConfiguration($variabileResultsBrands,'Non hai inserito la variabile contente i risultati della chiamata a Api Brand');
+        
+        $postParamNamePage='';
+        $postParamNamePerPageRecord='';
+        $postParmNumRecordPerPage=0;
+        $postParamDataUpdateName=null;
+        
+        
+        
+        
+            
+        
+    }
+    protected function getCsvConfiguration(){
+
+    }
+
+    protected function checkEmptyConfiguration($configValue,$configName){
+        if(empty(trim($configValue))){
+            throw new AppException($configName.' : vuoto o non valido');
+        }
+
     }
 }
